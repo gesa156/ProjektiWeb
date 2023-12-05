b8d951b8d7967ed45074bc2b116185548c536bfc
DELL <es68682@ubt-uni.net>
2023-12-05 02:08:42 +0100

    e shtova nje pjese per register form

---
+++ script.js
@@ -0,0 +1,8 @@
+const cc = document.querySelector('.cc');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click',()=>{
+    cc.classList.add('active');
+    
+})
