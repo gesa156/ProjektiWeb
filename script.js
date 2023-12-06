846b0f8c6c7a5a2e509c53cb26b1c3b352a90c6a
unknown <om64842@ubt-uni.net>
2023-12-06 02:30:51 +0100

    ndryshova edhe shtova kod

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const cc = document.querySelector('.cc');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+
 
 registerLink.addEventListener('click',()=>{
     cc.classList.add('active');
     
-})
+});
+
+loginLink.addEventListener('click',()=>{
+    cc.classList.remove('active');
+    
+});
+
+
+btnPopup.addEventListener('click',()=>{
+    cc.classList.add('active-popup');
+    
+});
+
+
+
