f95682e38e28712070c15fbf48f1138a823eef75
unknown <om64842@ubt-uni.net>
2023-12-07 03:13:07 +0100

    krijova footer-in

--- script.js
+++ script.js
@@ -21,4 +21,22 @@ btnPopup.addEventListener('click',()=>{
 });
 
 
+function validateForm(event) {
+    event.preventDefault(); // Prevent the form from submitting by default
+
+    // You can add your validation logic here
+    var email = document.getElementById('emailInput').value;
+    var password = document.getElementById('passwordInput').value;
+
+    // Example validation: Check if email and password are not empty
+    if (email.trim() === '' || password.trim() === '') {
+        alert('Please enter both email and password.');
+    } else {
+        // If validation passes, you can submit the form or perform other actions
+        alert('Form submitted successfully!');
+        // Uncomment the following line to submit the form
+        // document.getElementById('loginForm').submit();
+    }
+}
+
 
